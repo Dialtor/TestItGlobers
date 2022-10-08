67c05217881506afcb4d625a148321d6a1323137
Dialtor <dialtordev@gmail.com>
2022-10-08 08:40:12 -0500

    [create] Creando Navbar, y definiendo estilos

--- src/common/Navbar/Navbar.tsx
+++ src/common/Navbar/Navbar.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './styles/Navbar.module.css';
+import logoNav from '../../assets/books.png';
+import { Link } from 'react-router-dom';
+import {MdOutlineSearch} from 'react-icons/md';
 export interface NavbarInterface {}
 
 const Navbar : React.FC<NavbarInterface> = () => {
 	return (
 		<nav className={styles.navbar}>
-				<h1>Test IT Globers</h1>
+				<Link to={"/"}><img src={logoNav} width="60%" alt="logoNavbar" /></Link>
+
+				<div className={styles.search_container}>
+						<input  className={styles.search_input} type="text" placeholder='Search Title, Author or ISBN' />
+						<button><MdOutlineSearch style={{fontSize: "26px"}}/></button>
+				</div>
 		</nav>
 	)
 };
